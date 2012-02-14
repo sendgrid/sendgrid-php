cd56f738588cb8345ce88180e8eec598fb3c7653
CJ Buchmann <cj.buchmann@gmail.com>
2012-02-13 16:20:28 -0800

    added tests for SendGrid main class

---
+++ Test/SendGridTest.php
@@ -0,0 +1,36 @@
+<?php
+
+class SendGridTest extends PHPUnit_Framework_TestCase
+{
+
+  public function testConstruction()
+  {
+    $sendgrid = new SendGrid("fake_username", "fake_password");
+
+    $this->assertEquals("SendGrid", get_class($sendgrid));
+  }
+
+  public function testInitializers()
+  {
+    $sendgrid = new SendGrid("fake_username", "fake_password");
+
+    // test the working initializers that we currently have
+    $smtp = $sendgrid->smtp;
+    $web = $sendgrid->web;
+
+    $this->assertEquals("SendGrid\Smtp", get_class($smtp));
+    $this->assertEquals("SendGrid\Web", get_class($web));
+
+    try 
+    {
+      $sendgrid->notanapi;
+    } 
+    catch (Exception $e) 
+    {
+      return;
+    }
+
+    $this->fail('A non object was instanciated');
+    
+  }
+}
