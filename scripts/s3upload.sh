74fd63338542065687e1469d58dfd9d26e9b9a12
scottmotte <scott@scottmotte.com>
2014-03-08 13:42:33 -0600

    Only upload main if in master branch

--- scripts/s3upload.sh
+++ scripts/s3upload.sh
@@ -3,7 +3,8 @@
 # From:
 # http://raamdev.com/2008/using-curl-to-upload-files-via-post-to-amazon-s3/
 
-GIT_VERSION=`git rev-parse HEAD`
+GIT_VERSION=`git rev-parse --short HEAD`
+GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 
 rm -rf vendor composer.lock
 composer install --no-dev
@@ -11,21 +12,7 @@ printf "<?php\nrequire 'vendor/autoload.php';\nrequire 'lib/SendGrid.php';\n?>" 
 cd ..
 zip -r sendgrid-php.zip sendgrid-php -x \*.git\* \*composer\* \*composer.json\* \*scripts\* \*test\* \*.travis.yml\*
 
-echo "CURRENT PATH"
-echo $S3_BUCKET
-echo $S3_POLICY
 echo $GIT_VERSION
-pwd
-
-curl -X POST \
-  -F "key=sendgrid-php/sendgrid-php.zip" \
-  -F "acl=public-read" \
-  -F "AWSAccessKeyId=$S3_ACCESS_KEY" \
-  -F "Policy=$S3_POLICY" \
-  -F "Signature=$S3_SIGNATURE" \
-  -F "Content-Type=application/zip" \
-  -F "file=@./sendgrid-php.zip" \
-  https://s3.amazonaws.com/$S3_BUCKET
 
 curl -X POST \
   -F "key=sendgrid-php/versions/sendgrid-php-$GIT_VERSION.zip" \
@@ -36,5 +23,18 @@ curl -X POST \
   -F "Content-Type=application/zip" \
   -F "file=@./sendgrid-php.zip" \
   https://s3.amazonaws.com/$S3_BUCKET
+
+if [ "$GIT_BRANCH" = "master" ]
+then 
+  curl -X POST \
+    -F "key=sendgrid-php/sendgrid-php.zip" \
+    -F "acl=public-read" \
+    -F "AWSAccessKeyId=$S3_ACCESS_KEY" \
+    -F "Policy=$S3_POLICY" \
+    -F "Signature=$S3_SIGNATURE" \
+    -F "Content-Type=application/zip" \
+    -F "file=@./sendgrid-php.zip" \
+    https://s3.amazonaws.com/$S3_BUCKET
+fi
 
 exit 0
