4899267531645d970675edc446f03fef20a5a931
scottmotte <scott@scottmotte.com>
2014-04-18 10:23:19 -0700

    Version bump 2.0.5

--- lib/SendGrid.php
+++ lib/SendGrid.php
@@ -1,7 +1,7 @@
 <?php
 
 class SendGrid {
-  const VERSION = "2.0.4";
+  const VERSION = "2.0.5";
 
   protected $namespace  = "SendGrid",
             $url        = "https://api.sendgrid.com/api/mail.send.json",
