76152374393b1968f4734e701e0b55fb725c5a13
Francisco Serrano <fserrano@gmail.com>
2014-06-10 13:05:50 -0400

    Improve code with static declarations and dynamic actions

--- lib/SendGrid/Report.php
+++ lib/SendGrid/Report.php
@@ -4,11 +4,13 @@ namespace SendGrid;
 
 class Report {
 
+  static $actions = array('get','delete','count','add');
+  static $modules = array('spamreports','blocks','bounces','invalidemails','unsubscribes');
+  static $parameters = array('date','days','limit','offset','type','email');
+  static $parameters_underscored = array('start_date'=>'startDate','end_date'=>'endDate'); // to comply with PSR, methods shouldn't have underscores
   public $module,
-         $modules,
          $format,
          $action,
-         $parameters,
          $date,
          $days,
          $start_date,
@@ -19,27 +21,23 @@ class Report {
          $email;
 
   public function __construct() {
-    $this->modules = array('spamreports','blocks','bounces','invalidemails','unsubscribes');
-    $this->parameters = array('date','days','limit','offset','type','email');
-    $this->parameters_underscored = array('start_date'=>'startDate','end_date'=>'endDate'); // to comply with PSR, methods shouldn't have underscores
     $this->format = 'json';
-    $this->action = 'get';
   }
 
   public function __call($method, $args=null) {
-    // set module with calls
-    if(in_array($method,$this->modules)){
-      $this->module = $method;
-      return $this;
-    }
+    // set action (defaults to get)
+    $this->action = 'get';
+    if(in_array($method,self::$actions))$this->action = $method;
+    // set module
+    if(in_array($method,self::$modules))$this->module = $method;
     // set parameter
-    if(in_array($method,array_merge($this->parameters,array_values($this->parameters_underscored)))){
+    if(in_array($method,array_merge(self::$parameters,array_values(self::$parameters_underscored)))){
       $method = $this->addUnderscore($method);
       $args = array_shift($args);
       if(empty($args)) $args = 1;
       $this->{$method} = $args;
-      return $this;
     }
+    return $this;
    }
 
   public function getUrl($url=null)
@@ -48,23 +46,17 @@ class Report {
     return $url;
   }
 
-  public function delete()
-  {
-    $this->action = 'delete';
-    return $this;
-  }
-
   public function addUnderscore($method)
   {
-      if(in_array($method,array_values($this->parameters_underscored))){
-        $method = str_replace(array_values($this->parameters_underscored),array_keys($this->parameters_underscored),$method);
+      if(in_array($method,array_values(self::$parameters_underscored))){
+        $method = str_replace(array_values(self::$parameters_underscored),array_keys(self::$parameters_underscored),$method);
       }
       return $method;
   }
 
   public function toWebFormat() {
     $web = array();
-    foreach($this->parameters as $parameter){
+    foreach(self::$parameters as $parameter){
       if($this->{$parameter})$web[$parameter]=$this->{$parameter};
     }
     return $web;
