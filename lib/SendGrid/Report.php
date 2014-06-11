c895ef7e30e6c856caee450a29625168c98dd77f
Francisco Serrano <fserrano@gmail.com>
2014-06-10 21:37:43 -0400

    Add Tests

--- lib/SendGrid/Report.php
+++ lib/SendGrid/Report.php
@@ -4,21 +4,11 @@ namespace SendGrid;
 
 class Report {
 
-  static $actions = array('get','delete','count','add');
   static $modules = array('spamreports','blocks','bounces','invalidemails','unsubscribes');
+  static $actions = array('get','delete','count','add');
   static $parameters = array('date','days','limit','offset','type','email');
-  static $parameters_underscored = array('start_date'=>'startDate','end_date'=>'endDate'); // to comply with PSR, methods shouldn't have underscores
-  public $module,
-         $format,
-         $action,
-         $date,
-         $days,
-         $start_date,
-         $end_date,
-         $limit,
-         $offset,
-         $type,
-         $email;
+  static $parameters_underscored = array('start_date'=>'startDate','end_date'=>'endDate','delete_all'=>'deleteAll'); // to comply with PSR, methods shouldn't have underscores
+  public $module, $action, $format;
 
   public function __construct() {
     $this->format = 'json';
@@ -31,13 +21,18 @@ class Report {
     // set module
     if(in_array($method,self::$modules))$this->module = $method;
     // set parameter
-    if(in_array($method,array_merge(self::$parameters,array_values(self::$parameters_underscored)))){
+    $parameters = array_merge(self::$parameters,array_values(self::$parameters_underscored));
+    if(in_array($method,$parameters)){
       $method = $this->addUnderscore($method);
       $args = array_shift($args);
       if(empty($args)) $args = 1;
       $this->{$method} = $args;
     }
     return $this;
+   }
+   public function getParameters()
+   {
+     return array_merge(self::$parameters,array_values(self::$parameters_underscored));
    }
 
   public function getUrl($url=null)
@@ -56,8 +51,9 @@ class Report {
 
   public function toWebFormat() {
     $web = array();
-    foreach(self::$parameters as $parameter){
-      if($this->{$parameter})$web[$parameter]=$this->{$parameter};
+    $parameters = array_merge(self::$parameters,array_keys(self::$parameters_underscored));
+    foreach($parameters as $parameter){
+      if(isset($this->{$parameter}))$web[$parameter]=$this->{$parameter};
     }
     return $web;
   }
