bf6cdfa5247ad76f205268ca7126846509aeae6e
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-07-07 12:47:40 -0700

    updated sendgrid obj to be similar to other libs
    - changed username/password to api_user/api_key
    - allowed api_user, api_key, url, and version to be accessed and changed from the sendgrid object
    
    this makes the sendgrid object to the sendgrid object in our node and python libraries

--- lib/SendGrid.php
+++ lib/SendGrid.php
@@ -4,23 +4,30 @@ class SendGrid {
   const VERSION = "2.0.6";
 
   protected $namespace  = "SendGrid",
-            $url        = "https://api.sendgrid.com/api/mail.send.json",
             $headers    = array('Content-Type' => 'application/json'),
-            $username,
-            $password,
             $options,
             $web;
+  public    $api_user,
+            $api_key,
+            $url,
+            $version = self::VERSION;
+
   
-  public function __construct($username, $password, $options=array("turn_off_ssl_verification" => false)) {
-    $this->username = $username;
-    $this->password = $password;
+  public function __construct($api_user, $api_key, $options=array()) {
+    $this->api_user = $api_user;
+    $this->api_key = $api_key;
+
+    if( !isset($options["turn_off_ssl_verification"]) ){
+      $options["turn_off_ssl_verification"] = false;
+    }
+
     $this->options  = $options;
   }
 
   public function send(SendGrid\Email $email) {
     $form             = $email->toWebFormat();
-    $form['api_user'] = $this->username; 
-    $form['api_key']  = $this->password; 
+    $form['api_user'] = $this->api_user; 
+    $form['api_key']  = $this->api_key; 
 
     // option to ignore verification of ssl certificate
     if (isset($this->options['turn_off_ssl_verification']) && $this->options['turn_off_ssl_verification'] == true) {
