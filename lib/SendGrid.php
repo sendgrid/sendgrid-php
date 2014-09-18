7500d7a3adb3bfe81ebe144020477cb3ac8ca362
Oleksandr Kosmynin <lekz@appsorama.com>
2014-09-18 12:21:43 +0300

    Added unsubscribe and did some refactoring

--- lib/SendGrid.php
+++ lib/SendGrid.php
@@ -1,64 +1,129 @@
 <?php
 
-class SendGrid {
-  const VERSION = "2.1.1";
+class SendGrid
+{
+    const VERSION = "2.1.1";
 
-  protected $namespace  = "SendGrid",
-            $headers    = array('Content-Type' => 'application/json'),
-            $options,
-            $web;
-  public    $api_user,
-            $api_key,
-            $url,
-            $version = self::VERSION;
+    const RESPONSE_SUCCESS_MSG  = 'success';
+    const RESPONSE_ERROR_MSG    = 'error';
 
-  
-  public function __construct($api_user, $api_key, $options=array()) {
-    $this->api_user = $api_user;
-    $this->api_key = $api_key;
+    /**
+     * @var int The API call had an error in the parameters. The error will be encoded in the body of the response.
+     */
+    const ERROR_TYPE_INCORRECT_PARAMS   = 400;
 
-    if( !isset($options["turn_off_ssl_verification"]) ){
-      $options["turn_off_ssl_verification"] = false;
+    /**
+     * @var int The API call was unsuccessful. You should retry later.
+     */
+    const ERROR_TYPE_SENDGRID_FAIL      = 500;
+
+    protected   $api_user,
+                $api_key,
+                $options = array(
+                    'protocol'                  => 'https',
+                    'host'                      => 'api.sendgrid.com',
+                    'port'                      => 443,
+                    'turn_off_ssl_verification' => false,
+                    'unirest'                   => null,
+                    'use_concise_answers'       => false
+                ),
+                $last_errors = array(),
+                $last_errors_type;
+
+    public function __construct($api_user, $api_key, $options = array())
+    {
+        $this->api_user = $api_user;
+        $this->api_key = $api_key;
+
+        foreach ($options as $key => $value) {
+            $this->options[$key] = $value;
+        }
+
+        if ($this->options['turn_off_ssl_verification'] == true) {
+            $this->getUnirest()->verifyPeer(false);
+        }
     }
-    
 
-    $protocol = isset($options['protocol']) ? $options['protocol'] : 'https';
-    $host = isset($options['host']) ? $options['host'] : 'api.sendgrid.com';
-    $port = isset($options['port']) ? $options['port'] : '';
-    $endpoint = isset($options['endpoint']) ? $options['endpoint'] : '/api/mail.send.json';
-
-    $this->url = isset($options['url']) ? $options['url'] : $protocol . "://" . $host . ($port ? ":" . $port : "") . $endpoint;
-
-    $this->options  = $options;
-  }
-
-  public function send(SendGrid\Email $email) {
-    $form             = $email->toWebFormat();
-    $form['api_user'] = $this->api_user; 
-    $form['api_key']  = $this->api_key; 
-
-    // option to ignore verification of ssl certificate
-    if (isset($this->options['turn_off_ssl_verification']) && $this->options['turn_off_ssl_verification'] == true) {
-      \Unirest::verifyPeer(false);
+    protected function getUnirest() {
+        return $this->options['unirest'] ? $this->options['unirest'] : new \Unirest;
     }
 
-    $response = \Unirest::post($this->url, array('User-Agent' => 'sendgrid/' . $this->version . ';php'), $form);
-
-    return $response->body;
-  }
-
-  public static function register_autoloader() {
-    spl_autoload_register(array('SendGrid', 'autoloader'));
-  }
-
-  public static function autoloader($class) {
-    // Check that the class starts with "SendGrid"
-    if ($class == 'SendGrid' || stripos($class, 'SendGrid\\') === 0) {
-      $file = str_replace('\\', '/', $class);
-
-      if (file_exists(dirname(__FILE__) . '/' . $file . '.php')) {
-        require_once(dirname(__FILE__) . '/' . $file . '.php');
-      }
+    protected function getUrlToEndpoint($endpoint) {
+        return sprintf("%s://%s:%d/api/%s", $this->options['protocol'], $this->options['host'], $this->options['port'], $endpoint);
     }
-  }
+
+    protected function getHeaders() {
+        return array('User-Agent' => 'sendgrid/' . self::VERSION . ';php');
+    }
+
+    /**
+     * @param \Unirest\HttpResponse $rawResp
+     * @return mixed
+     */
+    protected function getResponse(\Unirest\HttpResponse $rawResp) {
+        $body = $rawResp->body;
+        if (!$this->options['use_concise_answers']) {
+            return $body;
+        }
+        if (property_exists($body, 'message') && $body->message = self::RESPONSE_SUCCESS_MSG) {
+            return true;
+        }
+        if (property_exists($body, 'errors')) {
+            $this->last_errors = (array) $body->errors;
+        }
+        if (property_exists($body, 'error')) {
+            $this->last_errors = array($body->error);
+        }
+        if ($rawResp->code >= 400 && $rawResp->code < 500) {
+            $this->last_errors_type = self::ERROR_TYPE_INCORRECT_PARAMS;
+        } else if ($rawResp->code >= 500 && $rawResp->code < 600) {
+            $this->last_errors_type = self::ERROR_TYPE_SENDGRID_FAIL;
+        }
+        return false;
+    }
+
+    /**
+     * @return array
+     */
+    public function getLastErrors() {
+        return $this->last_errors;
+    }
+
+    /**
+     * @return int | null
+     */
+    public function getLastErrorsType() {
+        return $this->last_errors_type;
+    }
+
+    /**
+     * @param \SendGrid\Email $email
+     * @return mixed
+     */
+    public function send(SendGrid\Email $email)
+    {
+        $form = $email->toWebFormat();
+        $form['api_user'] = $this->api_user;
+        $form['api_key'] = $this->api_key;
+
+        $response = $this->getUnirest()->post($this->getUrlToEndpoint('mail.send.json'), $this->getHeaders(), $form);
+        return $this->getResponse($response);
+    }
+
+    /**
+     * @param string $email
+     * @return mixed
+     */
+    public function unsubscribe($email)
+    {
+        $form = array(
+            'api_user'  => $this->api_user,
+            'api_key'   => $this->api_key,
+            'email'     => $email
+        );
+
+        $response = $this->getUnirest()->post($this->getUrlToEndpoint('unsubscribes.add.json'), $this->getHeaders(), $form);
+        return $this->getResponse($response);
+    }
+
 }
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,19 +1,3 @@
-<?php 
+<?php
 
-include(dirname(dirname(__FILE__)) . '/lib/SendGrid.php');
-SendGrid::register_autoloader();
-
-function autoload_tests($class) {
-
-  if (strpos($class, 'SendGridTest_') !== 0) {
-    return;
-  }
-
-  $class = substr($class, 13);
-  $file = str_replace('_', '/', $class);
-  if (file_exists(dirname(__FILE__) . '/' . $file . '.php')) {
-    require_once(dirname(__FILE__) . '/' . $file . '.php');
-  }
-}
-
-spl_autoload_register('autoload_tests');
+require_once __DIR__.'/../vendor/autoload.php';
