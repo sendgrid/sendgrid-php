6b7fcbea97480d4f8daff1fa2af7b103d4557286
Eddie Zaneski <eddiezane@gmail.com>
2015-04-14 00:04:56 -0400

    PSR-2 Tests

--- test/SendGrid.php
+++ test/SendGrid.php
@@ -2,76 +2,86 @@
 
 use \Mockery as m;
 
-class SendGridTest_SendGrid extends PHPUnit_Framework_TestCase {
+class SendGridTest_SendGrid extends PHPUnit_Framework_TestCase
+{
 
-  public function tearDown() {
-    m::close();
-  }
+    public function tearDown()
+    {
+        m::close();
+    }
 
-  public function testVersion() {
-    $this->assertEquals(SendGrid::VERSION, '3.0.0-RC1');
-    $this->assertEquals(json_decode(file_get_contents('../composer.json'))->version, SendGrid::VERSION);
-  }
+    public function testVersion()
+    {
+        $this->assertEquals(SendGrid::VERSION, '3.0.0-RC1');
+        $this->assertEquals(json_decode(file_get_contents('../composer.json'))->version, SendGrid::VERSION);
+    }
 
-  public function testInitialization() {
-    $sendgrid = new SendGrid('user', 'pass');
-    $this->assertEquals('SendGrid', get_class($sendgrid));
-  }
+    public function testInitialization()
+    {
+        $sendgrid = new SendGrid('user', 'pass');
+        $this->assertEquals('SendGrid', get_class($sendgrid));
+    }
 
-  public function testDefaultURL() {
-    $sendgrid = new SendGrid('user', 'pass');
-    $this->assertEquals('https://api.sendgrid.com', $sendgrid->url);
-  }
+    public function testDefaultURL()
+    {
+        $sendgrid = new SendGrid('user', 'pass');
+        $this->assertEquals('https://api.sendgrid.com', $sendgrid->url);
+    }
 
-  public function testDefaultEndpoint() {
-    $sendgrid = new SendGrid('user', 'pass');
-    $this->assertEquals('/api/mail.send.json', $sendgrid->endpoint);
-    
-  }
+    public function testDefaultEndpoint()
+    {
+        $sendgrid = new SendGrid('user', 'pass');
+        $this->assertEquals('/api/mail.send.json', $sendgrid->endpoint);
 
-  public function testCustomURL() {
-    $options = array( 'protocol' => 'http', 'host' => 'sendgrid.org', 'endpoint' => '/send', 'port' => '80' );
-    $sendgrid = new SendGrid('user', 'pass', $options);
-    $this->assertEquals('http://sendgrid.org:80', $sendgrid->url);
-  }
+    }
 
-  public function testSwitchOffSSLVerification() {
-    $sendgrid = new SendGrid('foo', 'bar', array('turn_off_ssl_verification' => true));
-    $options = $sendgrid->getOptions();
-    $this->assertTrue(isset($options['turn_off_ssl_verification']));
-  }
+    public function testCustomURL()
+    {
+        $options = array( 'protocol' => 'http', 'host' => 'sendgrid.org', 'endpoint' => '/send', 'port' => '80' );
+        $sendgrid = new SendGrid('user', 'pass', $options);
+        $this->assertEquals('http://sendgrid.org:80', $sendgrid->url);
+    }
 
-  /**
-   * @expectedException SendGrid\Exception
-   */
-  public function testSendGridExceptionThrownWhenNot200() {
-    $mockResponse = (object)array('code' => 400, 'raw_body' => "{'message': 'error', 'errors': ['Bad username / password']}");
+    public function testSwitchOffSSLVerification()
+    {
+        $sendgrid = new SendGrid('foo', 'bar', array('turn_off_ssl_verification' => true));
+        $options = $sendgrid->getOptions();
+        $this->assertTrue(isset($options['turn_off_ssl_verification']));
+    }
 
-    $sendgrid = m::mock('SendGrid[postRequest]', array('foo', 'bar'));
-    $sendgrid->shouldReceive('postRequest')->once()->andReturn($mockResponse);
+    /**
+     * @expectedException SendGrid\Exception
+     */
+    public function testSendGridExceptionThrownWhenNot200()
+    {
+        $mockResponse = (object)array('code' => 400, 'raw_body' => "{'message': 'error', 'errors': ['Bad username / password']}");
 
-    $email = new SendGrid\Email();
-    $email->setFrom('bar@foo.com')->
-            setSubject('foobar subject')->
-            setText('foobar text')->
-            addTo('foo@bar.com');
+        $sendgrid = m::mock('SendGrid[postRequest]', array('foo', 'bar'));
+        $sendgrid->shouldReceive('postRequest')->once()->andReturn($mockResponse);
 
-    $response = $sendgrid->send($email);
-  }
+        $email = new SendGrid\Email();
+        $email->setFrom('bar@foo.com')
+            ->setSubject('foobar subject')
+            ->setText('foobar text')
+            ->addTo('foo@bar.com');
 
-  public function testSendGridExceptionNotThrownWhen200() {
-    $mockResponse = (object)array('code' => 200, 'body' => (object)array('message' => 'success'));
+        $response = $sendgrid->send($email);
+    }
 
-    $sendgrid = m::mock('SendGrid[postRequest]', array('foo', 'bar'));
-    $sendgrid->shouldReceive('postRequest')->once()->andReturn($mockResponse);
+    public function testSendGridExceptionNotThrownWhen200()
+    {
+        $mockResponse = (object)array('code' => 200, 'body' => (object)array('message' => 'success'));
 
-    $email = new SendGrid\Email();
-    $email->setFrom('bar@foo.com')->
-            setSubject('foobar subject')->
-            setText('foobar text')->
-            addTo('foo@bar.com');
+        $sendgrid = m::mock('SendGrid[postRequest]', array('foo', 'bar'));
+        $sendgrid->shouldReceive('postRequest')->once()->andReturn($mockResponse);
 
-    $response = $sendgrid->send($email);
-  }
+        $email = new SendGrid\Email();
+        $email->setFrom('bar@foo.com')
+            ->setSubject('foobar subject')
+            ->setText('foobar text')
+            ->addTo('foo@bar.com');
+
+        $response = $sendgrid->send($email);
+    }
 }
 
