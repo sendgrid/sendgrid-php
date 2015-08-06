2a8eb85325b6747b6039c119cf86b436d4260a6e
Brady Owens <info@fastglass.net>
2015-08-06 20:53:11 +0200

    Update to GuzzleHttp 6.0.x. Most of the changes are in the construct and prepareHttpClient. The other changes are formatting and styling. Changed all null's to NULL.

--- lib/SendGrid.php
+++ lib/SendGrid.php
@@ -1,7 +1,8 @@
 <?php
 
-class SendGrid
-{
+use GuzzleHttp\Exception\ClientException;
+
+class SendGrid {
     const VERSION = '3.2.0';
 
     protected
@@ -17,38 +18,39 @@ class SendGrid
         $endpoint,
         $version = self::VERSION;
 
-    public function __construct($apiUserOrKey, $apiKeyOrOptions = null, $options = array())
-    {
-        // Check if given a username + password or api key
+    public function __construct($apiUserOrKey, $apiKeyOrOptions = NULL, $options = array()) {
+        // Check if given a username + password or api key.
         if (is_string($apiKeyOrOptions)) {
-            // Username and password
+            // Username and password.
             $this->apiUser = $apiUserOrKey;
             $this->apiKey = $apiKeyOrOptions;
             $this->options = $options;
-        } elseif (is_array($apiKeyOrOptions) || $apiKeyOrOptions === null) {
-            // API key
+        }
+        elseif (is_array($apiKeyOrOptions) || $apiKeyOrOptions === NULL) {
+            // API key.
             $this->apiKey = $apiUserOrKey;
-            $this->apiUser = null;
+            $this->apiUser = NULL;
 
-            // With options
+            // With options.
             if (is_array($apiKeyOrOptions)) {
                 $this->options = $apiKeyOrOptions;
             }
-        } else {
+        }
+        else {
             // Won't be thrown?
             throw new InvalidArgumentException('Need a username + password or api key!');
         }
 
-        $this->options['turn_off_ssl_verification'] = (isset($this->options['turn_off_ssl_verification']) && $this->options['turn_off_ssl_verification'] == true);
+        $this->options['turn_off_ssl_verification'] = (isset($this->options['turn_off_ssl_verification']) && $this->options['turn_off_ssl_verification'] == TRUE);
         if (!isset($this->options['raise_exceptions'])) {
-            $this->options['raise_exceptions'] = true;
+            $this->options['raise_exceptions'] = TRUE;
         }
         $protocol = isset($this->options['protocol']) ? $this->options['protocol'] : 'https';
-        $host = isset($this->options['host']) ? $this->options['host'] : 'api.sendgrid.com';
+        $host = isset($this->options['host']) ? $this->options['host'] : 'api.sendgrid.com/api/';
         $port = isset($this->options['port']) ? $this->options['port'] : '';
 
         $this->url = isset($this->options['url']) ? $this->options['url'] : $protocol . '://' . $host . ($port ? ':' . $port : '');
-        $this->endpoint = isset($this->options['endpoint']) ? $this->options['endpoint'] : '/api/mail.send.json';
+        $this->endpoint = isset($this->options['endpoint']) ? $this->options['endpoint'] : 'mail.send.json';
 
         $this->client = $this->prepareHttpClient();
     }
@@ -58,31 +60,36 @@ class SendGrid
      *
      * @return \GuzzleHttp\Client
      */
-    private function prepareHttpClient()
-    {
+    private function prepareHttpClient() {
         $headers = array();
-        $headers['verify'] = !$this->options['turn_off_ssl_verification'];
+        // $headers['verify'] = !$this->options['turn_off_ssl_verification'];
         // Using api key
-        if ($this->apiUser === null) {
-            $headers['Authorizaton'] = 'Bearer' . ' ' . $this->apiKey;
+        if ($this->apiUser === NULL) {
+            $headers['Authorization'] = 'Bearer' . ' ' . $this->apiKey;
         }
+        // @TODO Handle a username and password.
 
         // Using http proxy
         if (isset($this->options['proxy'])) {
             $headers['proxy'] = $this->options['proxy'];
         }
         $headers['User-Agent'] = 'sendgrid/' . $this->version . ';php';
+        // Create an empty stack for error processing.
+        // Guzzlehttp will choose the most appropriate handler based on the system.
+        $stack = \GuzzleHttp\HandlerStack::create();
 
-        $client = new \GuzzleHttp\Client(['base_uri' => $this->url, 'headers' => [$headers]]);
-
+        $client = new \GuzzleHttp\Client([
+            'base_uri' => $this->url,
+            'headers' => $headers,
+            'handler' => $stack
+        ]);
         return $client;
     }
 
     /**
      * @return array The protected options array
      */
-    public function getOptions()
-    {
+    public function getOptions() {
         return $this->options;
     }
 
@@ -94,12 +101,12 @@ class SendGrid
      * @throws SendGrid\Exception if the response code is not 200
      * @return stdClass SendGrid response object
      */
-    public function send(SendGrid\Email $email)
-    {
+    public function send(SendGrid\Email $email) {
         $form = $email->toWebFormat();
-
+        //$this->request = new \GuzzleHttp\Psr7\Request('POST', $this->url . $this->endpoint);
+        // @TODO Add username/password to the header.
         // Using username password
-        if ($this->apiUser !== null) {
+        if ($this->apiUser !== NULL) {
             $form['api_user'] = $this->apiUser;
             $form['api_key'] = $this->apiKey;
         }
@@ -121,24 +128,24 @@ class SendGrid
      *
      * @return SendGrid\Response
      */
-    public function postRequest($endpoint, $form)
-    {
-        $req = $this->client->post($endpoint, null, $form);
-
-        $res = $req->send();
-
-        $response = new SendGrid\Response($res->getStatusCode(), $res->getHeaders(), $res->getBody(true), $res->json());
+    public function postRequest($endpoint, $form) {
+        try {
+            $res = $this->client->post($endpoint, ['query' => $form]);
+        }
+        catch (GuzzleHttp\Exception\ClientException $e) {
+            echo 'Sendgrid API has experienced and error completing your request.';
+            var_dump($e);
+        }
+        $response = new SendGrid\Response($res->getStatusCode(), $res->getHeaders(), $res->getBody(TRUE), json_decode($res->getBody(TRUE)));
 
         return $response;
     }
 
-    public static function register_autoloader()
-    {
+    public static function register_autoloader() {
         spl_autoload_register(array('SendGrid', 'autoloader'));
     }
 
-    public static function autoloader($class)
-    {
+    public static function autoloader($class) {
         // Check that the class starts with 'SendGrid'
         if ($class == 'SendGrid' || stripos($class, 'SendGrid\\') === 0) {
             $file = str_replace('\\', '/', $class);
