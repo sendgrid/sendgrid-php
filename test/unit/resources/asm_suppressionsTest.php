10701c3d24f274b40fdbed5b625971edb1b5c675
Elmer Thomas <elmer@thinkingserious.com>
2015-10-16 12:11:51 -0700

    Added ASM Suppressions DELETE

--- test/unit/resources/asm_suppressionsTest.php
+++ test/unit/resources/asm_suppressionsTest.php
@@ -26,6 +26,7 @@ class SendGridTest_ASMSuppressions extends GuzzleTestCase
     $this->assertEquals(200, $response->getStatusCode());
     $this->assertEquals('["elmer.thomas+test-add-unsub@gmail.com","elmer.thomas+test1@gmail.com"]', $response->getBody());
   }
+  
   public function testPOST()
   { 
     $response = new Response(
@@ -63,5 +64,20 @@ class SendGridTest_ASMSuppressions extends GuzzleTestCase
     $response = $sendgrid->asm_suppressions->post($group_id, $email);
     $this->assertEquals(201, $response->getStatusCode());
     $this->assertEquals('{"recipient_emails":["elmer.thomas+test2@gmail.com","elmer.thomas+test3@gmail.com"]}', $response->getBody());
+  }
+  
+  public function testDELETE()
+  { 
+    $response = new Response(204,'');
+    
+    $client = new HTTPClient('https://api.sendgrid.com');
+    $mock = new MockPlugin();
+    $mock->addResponse($response);
+    $client->addSubscriber($mock);
+    $sendgrid = new Client('sendgrid_apikey');
+    $sendgrid->setClient($client);
+    $response = $sendgrid->asm_suppressions->delete(70, "elmer.thomas+test1@gmail.com");
+    $this->assertEquals(204, $response->getStatusCode());
+    $this->assertEquals('', $response->getBody());
   }
 }
