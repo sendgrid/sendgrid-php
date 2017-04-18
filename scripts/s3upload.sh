fd1737ef7d0dc5ddff7985373b4d7682635b87e1
Elmer Thomas <elmer@thinkingserious.com>
2017-04-18 09:23:49 -0700

    Don't include Prism binary in upload

--- scripts/s3upload.sh
+++ scripts/s3upload.sh
@@ -9,7 +9,7 @@ rm -rf vendor composer.lock
 composer install --no-dev
 printf "<?php\nrequire __DIR__ . '/vendor/autoload.php';\n?>" > sendgrid-php.php
 cd ..
-zip -r sendgrid-php.zip sendgrid-php -x \*.git\* \*composer.json\* \*scripts\* \*test\* \*.travis.yml\*
+zip -r sendgrid-php.zip sendgrid-php -x \*.git\* \*composer.json\* \*scripts\* \*test\* \*.travis.yml\* \*prism\*
 
 curl -X POST \
   -F "key=sendgrid-php/versions/sendgrid-php-$GIT_VERSION.zip" \
