b300612f363265bdc0a5805262fa385f9e2fe872
Joseph Opanel <opanelj@yahoo.com>
2017-10-11 10:33:20 -0700

    Auto Generate Plain Text Content from HTML
    If type is text/plain then it converts any HTML to plain text.

--- lib/helpers/mail/Mail.php
+++ lib/helpers/mail/Mail.php
@@ -741,8 +741,40 @@ class Content implements \JsonSerializable
 
     public function __construct($type, $value)
     {
+        if ($type == 'text/plain') {
+        $this->type = $this->convertPlainText($type);
+        }
         $this->type = $type;
         $this->value = mb_convert_encoding($value, 'UTF-8', 'UTF-8');
+    }
+    
+    public function convertPlainText($type) {
+	$type = preg_replace(
+        array(
+            '@<head[^>]*?>.*?</head>@siu',
+            '@<style[^>]*?>.*?</style>@siu',
+            '@<script[^>]*?.*?</script>@siu',
+            '@<object[^>]*?.*?</object>@siu',
+            '@<embed[^>]*?.*?</embed>@siu',
+            '@<applet[^>]*?.*?</applet>@siu',
+            '@<noframes[^>]*?.*?</noframes>@siu',
+            '@<noscript[^>]*?.*?</noscript>@siu',
+            '@<noembed[^>]*?.*?</noembed>@siu',
+            '@</?((address)|(blockquote)|(center)|(del))@iu',
+            '@</?((div)|(h[1-9])|(ins)|(isindex)|(p)|(pre))@iu',
+            '@</?((dir)|(dl)|(dt)|(dd)|(li)|(menu)|(ol)|(ul))@iu',
+            '@</?((table)|(th)|(td)|(caption))@iu',
+            '@</?((form)|(button)|(fieldset)|(legend)|(input))@iu',
+            '@</?((label)|(select)|(optgroup)|(option)|(textarea))@iu',
+            '@</?((frameset)|(frame)|(iframe))@iu',
+        ),
+        array(
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            "\n\$0", "\n\$0", "\n\$0", "\n\$0", "\n\$0", "\n\$0",
+            "\n\$0", "\n\$0",
+        ),
+        $type ); 
+		return strip_tags($type);
     }
 
     public function setType($type)
