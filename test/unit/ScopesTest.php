14e914610a468b51d93364c944db1665cdf92f00
pxgamer <owzie123@gmail.com>
2017-10-30 16:19:30 +0000

    Added ScopesTest class

---
+++ test/unit/ScopesTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SendGridPhp\Tests;
+
+class ScopesTest extends BaseTestClass
+{
+    public function testScopesGetMethod()
+    {
+        $request_headers = ["X-Mock: 200"];
+        $response = self::$sg->client->scopes()->get(null, null, $request_headers);
+        $this->assertEquals(200, $response->statusCode());
+    }
+}
