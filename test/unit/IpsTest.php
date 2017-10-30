8900a92af41935ad55ae09b3e48a29b33c31ae02
pxgamer <owzie123@gmail.com>
2017-10-30 15:57:20 +0000

    Added IpsTest class

---
+++ test/unit/IpsTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SendGridPhp\Tests;
+
+class IpsTest extends BaseTestClass
+{
+    public function testIpsGetMethod()
+    {
+        $query_params = json_decode('{"subuser": "test_string", "ip": "test_string", "limit": 1, "exclude_whitelabels": "true", "offset": 1}');
+        $request_headers = ["X-Mock: 200"];
+        $response = self::$sg->client->ips()->get(null, $query_params, $request_headers);
+        $this->assertEquals(200, $response->statusCode());
+    }
+
+    public function testIpsAssignedGetMethod()
+    {
+        $request_headers = ["X-Mock: 200"];
+        $response = self::$sg->client->ips()->assigned()->get(null, null, $request_headers);
+        $this->assertEquals(200, $response->statusCode());
+    }
+}
