5b7173e58472eac181fc4e8fbf19e985b95f1346
pxgamer <owzie123@gmail.com>
2017-10-30 16:39:22 +0000

    Added SuppressionUnsubscribesTest class

---
+++ test/unit/SuppressionUnsubscribesTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SendGridPhp\Tests;
+
+class SuppressionUnsubscribesTest extends BaseTestClass
+{
+    public function testSuppressionUnsubscribesGetMethod()
+    {
+        $query_params = json_decode('{"start_time": 1, "limit": 1, "end_time": 1, "offset": 1}');
+        $request_headers = ["X-Mock: 200"];
+        $response = self::$sg->client->suppression()->unsubscribes()->get(null, $query_params, $request_headers);
+        $this->assertEquals(200, $response->statusCode());
+    }
+}
