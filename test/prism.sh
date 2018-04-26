06ef71cf38e4061538febf5d7dd7333be512579c
Elmer Thomas <elmer@thinkingserious.com>
2018-04-26 10:58:39 -0700

    Make sure prism runs in Travis

--- test/prism.sh
+++ test/prism.sh
@@ -41,4 +41,17 @@ else
 fi
 }
 
-install
+run () {
+  echo "Running prism..."
+  cd ../prism/bin
+  ./prism run --mock --spec https://raw.githubusercontent.com/sendgrid/sendgrid-oai/master/oai_stoplight.json
+}
+
+if [ -f ../prism/bin/prism ]; then
+   echo "Prism is already installed."
+   run
+else
+   echo "Prism is not installed."
+   install
+   run
+fi
