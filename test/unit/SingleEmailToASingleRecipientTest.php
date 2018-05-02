5ac836dd7a197e142dea2dfafad244590b66c7c8
Elmer Thomas <elmer@thinkingserious.com>
2018-05-02 06:24:56 -0700

    Added initial unit tests, fixed a few bugs

--- test/unit/SingleEmailToASingleRecipientTest.php
+++ test/unit/SingleEmailToASingleRecipientTest.php
@@ -3,10 +3,12 @@
 namespace SendGrid\Tests;
 
 use SendGrid\Tests\BaseTestClass;
+use Swaggest\JsonDiff\JsonDiff;
+use Swaggest\JsonDiff\JsonPatch;
 
 class SingleEmailToASingleRecipientTest extends BaseTestClass
 {
-    private $REQUEST_OBJECT = '{"personalizations":[{"subject":"Sending with SendGrid is Fun"},{"to":[{"name":"test@example.com","email":"Example User"}]}],"from":{"name":"test@example.com","email":"Example User"},"content":[{"type":"text\/plain","value":"and easy to do anywhere, even with PHP"},{"type":"text\/html","value":"<strong>and easy to do anywhere, even with PHP<\/strong>"}]}';
+    private $REQUEST_OBJECT = '{"content":[{"type":"text/plain","value":"and easy to do anywhere, even with PHP"},{"type":"text/html","value":"<strong>and easy to do anywhere, even with PHP</strong>"}],"from":{"email":"Example User","name":"test@example.com"},"personalizations":[{"subject":"Sending with SendGrid is Fun","to":[{"email":"Example User","name":"test@example.com"}]}]}';
 
     public function testWithObjects()
     {
@@ -24,21 +26,26 @@ class SingleEmailToASingleRecipientTest extends BaseTestClass
         );
         $json = json_encode($email->jsonSerialize($this->REQUEST_OBJECT));
 
-        $this->assertEquals(
-            $json,
-            $this->REQUEST_OBJECT
-        );
+        $diff = new JsonDiff(json_decode($json), json_decode($this->REQUEST_OBJECT), JsonDiff::REARRANGE_ARRAYS);
+        $patch = $diff->getPatch();
+        $patch_array = JsonPatch::export($patch);
+        $this->assertTrue(empty($patch_array));
     }
 
-    // public function testWithoutObjects()
-    // {
-    //     $email = new \SendGrid\Mail\Mail(); 
-    //     $email->setFrom("Example User", "test@example.com");
-    //     $email->setSubject("Sending with SendGrid is Fun");
-    //     $email->addTo("Example User", "test@example.com");
-    //     $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-    //     $email->addContent("text/html", "<strong>and easy to do anywhere, even with PHP</strong>");
+    public function testWithoutObjects()
+    {
+        $email = new \SendGrid\Mail\Mail(); 
+        $email->setFrom("Example User", "test@example.com");
+        $email->setSubject("Sending with SendGrid is Fun");
+        $email->addTo("Example User", "test@example.com");
+        $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+        $email->addContent("text/html", "<strong>and easy to do anywhere, even with PHP</strong>");
 
-    //     $json = json_encode($email->jsonSerialize());
-    // }
+        $json = json_encode($email->jsonSerialize());
+
+        $diff = new JsonDiff(json_decode($json), json_decode($this->REQUEST_OBJECT), JsonDiff::REARRANGE_ARRAYS);
+        $patch = $diff->getPatch();
+        $patch_array = JsonPatch::export($patch);
+        $this->assertTrue(empty($patch_array));
+    }
 }
