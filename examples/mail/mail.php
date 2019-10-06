e61e74ffe4f76cf48d86b46cde4fd4c9e7c98fed
Chris Deaton <flashadvocate@gmail.com>
2019-10-06 14:18:52 -0400

    fix mail helper readme link

--- examples/mail/mail.php
+++ examples/mail/mail.php
@@ -39,7 +39,7 @@ try {
 ////////////////////////////////////////////////////
 // v3 Mail Send #
 // POST /mail/send #
-// This endpoint has a helper, check it out [here](https://github.com/sendgrid/sendgrid-php/blob/master/lib/helpers/mail/README.md).
+// This endpoint has a helper, check it out [here](https://github.com/sendgrid/sendgrid-php/blob/master/lib/mail/README.md).
 
 $request_body = json_decode('{
   "asm": {
