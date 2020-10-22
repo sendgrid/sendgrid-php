f4828bb96ffaa113a34255ffa462f326490819d4
Eva Beekers <EvaLok@users.noreply.github.com>
2020-10-22 23:08:45 +0200

    register autoload function if not using composer

--- sendgrid-php.php
+++ sendgrid-php.php
@@ -11,10 +11,33 @@ $composerAutoloadFileExists = (is_file($composerAutoloadFile));
 
 // Can't locate SendGrid\Mail\Mail class?
 if (!class_exists(Mail::class)) {
-    // Suggest to load Composer autoloader of project
+    // if the user is not using composer, register an autoload function to load based on relative directory structure
     if (!$composerAutoloadFileExists) {
-        //  Can't load the Composer autoloader in this project folder
-        error_log("Composer autoloader not found. Execute 'composer install' in the project folder first.");
+        require_once( __DIR__ . '/lib/BaseSendGridClientInterface.php');
+        require_once( __DIR__ . '/lib/SendGrid.php');
+        require_once( __DIR__ . '/lib/TwilioEmail.php');
+
+        // autoload based on directory structure & without composer
+        spl_autoload_register(function($class){
+            if ( starts_with($class, 'SendGrid\\') ){
+                $path = str_replace('\\', '/', $class);
+
+                $pieces = explode('/', $path);
+                array_shift($pieces); // we don't use the first piece (SendGrid)
+                $file = array_pop($pieces);
+
+                $dirPath = __DIR__ . '/lib/' . strtolower(implode('/', $pieces));
+                $filePath = $dirPath . '/' . $file . '.php';
+
+                if ( file_exists($filePath) ) {
+                    require_once($filePath);
+                }
+
+                if ( ! class_exists($class) ) {
+                    error_log("Error finding SendGrid class [$class] (expected [$filePath]). Please review your autoloading configuration.");
+                }
+            }
+        });
     } else {
         // Load Composer autoloader
         require_once $composerAutoloadFile;
