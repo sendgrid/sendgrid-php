8ff944fe66e7022e81994f27943d7baf01f9004a
Florin Marina <mflorin1976@gmail.com>
2024-06-06 15:20:56 +0300

    updates test unit class

--- test/unit/ReplyToListTest.php
+++ test/unit/ReplyToListTest.php
@@ -1,7 +1,10 @@
 <?php
 
+namespace SendGrid\Tests\Unit;
+
 use PHPUnit\Framework\TestCase;
 use SendGrid\Mail\Mail;
+use SendGrid\Mail\ReplyTo;
 
 class ReplyToListTest extends TestCase
 {
@@ -10,16 +13,16 @@ class ReplyToListTest extends TestCase
         $emailObject = new Mail();
         $replyToList = [
             new ReplyTo('test1@example.com', 'Test1'),
-            new ReplyTo('test2@example.com', 'Test2')
+            new ReplyTo('test2@example.com', 'Test2'),
         ];
 
         $emailObject->setReplyToList($replyToList);
 
-        $this->assertCount(2, $emailObject->reply_to_list);
-        $this->assertEquals('test1@example.com', $emailObject->reply_to_list[0]->getEmail());
-        $this->assertEquals('Test1', $emailObject->reply_to_list[0]->getName());
-        $this->assertEquals('test2@example.com', $emailObject->reply_to_list[1]->getEmail());
-        $this->assertEquals('Test2', $emailObject->reply_to_list[1]->getName());
+        $this->assertCount(2, $emailObject->getReplyToList());
+        $this->assertEquals('test1@example.com', $emailObject->getReplyToList()[0]->getEmail());
+        $this->assertEquals('Test1', $emailObject->getReplyToList()[0]->getName());
+        $this->assertEquals('test2@example.com', $emailObject->getReplyToList()[1]->getEmail());
+        $this->assertEquals('Test2', $emailObject->getReplyToList()[1]->getName());
     }
 
     public function testSetReplyToListWithValidReplyToArray()
@@ -27,16 +30,16 @@ class ReplyToListTest extends TestCase
         $emailObject = new Mail();
         $replyToList = [
             ['email' => 'test1@example.com', 'name' => 'Test1'],
-            ['email' => 'test2@example.com']
+            ['email' => 'test2@example.com'],
         ];
 
         $emailObject->setReplyToList($replyToList);
 
-        $this->assertCount(2, $emailObject->reply_to_list);
-        $this->assertEquals('test1@example.com', $emailObject->reply_to_list[0]->getEmail());
-        $this->assertEquals('Test1', $emailObject->reply_to_list[0]->getName());
-        $this->assertEquals('test2@example.com', $emailObject->reply_to_list[1]->getEmail());
-        $this->assertNull($emailObject->reply_to_list[1]->getName());
+        $this->assertCount(2, $emailObject->getReplyToList());
+        $this->assertEquals('test1@example.com', $emailObject->getReplyToList()[0]->getEmail());
+        $this->assertEquals('Test1', $emailObject->getReplyToList()[0]->getName());
+        $this->assertEquals('test2@example.com', $emailObject->getReplyToList()[1]->getEmail());
+        $this->assertNull($emailObject->getReplyToList()[1]->getName());
     }
 
     public function testSetReplyToListWithInvalidReplyToArray()
@@ -47,7 +50,7 @@ class ReplyToListTest extends TestCase
         $this->expectExceptionMessage('Email is mandatory on ReplyToList array.');
 
         $replyToList = [
-            ['name' => 'Test1']
+            ['name' => 'Test1'],
         ];
 
         $emailObject->setReplyToList($replyToList);
@@ -57,24 +60,27 @@ class ReplyToListTest extends TestCase
     {
         $emailObject = new Mail();
         $replyToList = [
-            'test1@example.com'
+            'test1@example.com',
         ];
 
         $emailObject->setReplyToList($replyToList);
 
-        $this->assertCount(1, $emailObject->reply_to_list);
-        $this->assertEquals('test1@example.com', $emailObject->reply_to_list[0]->getEmail());
-        $this->assertNull($emailObject->reply_to_list[0]->getName());
+        $this->assertCount(1, $emailObject->getReplyToList());
+        $this->assertEquals('test1@example.com', $emailObject->getReplyToList()[0]->getEmail());
+        $this->assertNull($emailObject->getReplyToList()[0]->getName());
     }
 
     public function testSetReplyToListWithMoreThanMaxItems()
     {
-        $emailObject = new Mail();
+        $emailObject = new \SendGrid\Mail\Mail();
+        // Create a replyToList with 1001 items
         $replyToList = array_fill(0, 1001, 'test@example.com');
 
-        $this->expectException(\InvalidArgumentException::class);
-        $this->expectExceptionMessage('Number of items in "replyToList" must be at most 1000.');
+        // Update to expect the correct exception type
+        $this->expectException(\SendGrid\Mail\TypeException::class);
+        $this->expectExceptionMessage('Number of elements in "$replyToList" can not be more than 1000.');
 
+        // Call the method that is expected to throw the exception
         $emailObject->setReplyToList($replyToList);
     }
 }
