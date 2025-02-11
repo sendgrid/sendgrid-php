8aa23256f4a5482822ae7467608dd0a5a5a95d45
Twilio <team_interfaces+github@twilio.com>
2025-02-11 10:49:29 +0000

    Release 8.1.10

--- lib/BaseSendGridClientInterface.php
+++ lib/BaseSendGridClientInterface.php
@@ -12,7 +12,7 @@ use SendGrid\Response;
 abstract class BaseSendGridClientInterface
 {
     /** @var string SendGrid API library version */
-    const VERSION = '8.1.9';
+    const VERSION = '8.1.10';
 
     /** @var Client SendGrid HTTP Client library */
     public $client;
